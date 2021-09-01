f344b30278d0039bf317abafa4f29a7ab7b7c110
ToMattBan <cleyton_laz@protonmail.com>
2021-09-01 13:36:41 -0300

    GH pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.scss';
 import Products from "./Assets/products.json"
 
 function App() {
   return (
-    <div>
-      {Products.map(product => (
-        <p>
+    <div style={{'display': 'none'}}>
+      {Products.map((product, index) => (
+        <h4 key={product.Id}>
           <div>Id - { product.Id }</div>
           <div>img - { product.img }</div>
           <div>Url - { product.Url }</div>
           <div>Name - { product.Name }</div>
           <div>Price - { product.Price }</div>
-        </p>
+        </h4>
       ))}
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,40 @@
-function Header() {
-    return (
-        <h1>Aqui é o Header</h1>
-    )
-}
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import { Avatar } from '@material-ui/core';
+import useStyles from "./ComponentStyle.js"
 
-export default Header;
+export default function Header() {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.headerToolbars}>
+                <Typography className={classes.headerLogoTitle} variant="h6" noWrap>
+                    Lojenha Online
+                </Typography>
+                <div className={'_df'}>
+                    <span>Mathias</span>
+                    <Avatar style={{backgroundColor: 'greeen'}}>M</Avatar>
+                </div>
+            </Toolbar>
+            <Toolbar className={classes.headerToolbars}>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon />
+                    </div>
+                    <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+}
